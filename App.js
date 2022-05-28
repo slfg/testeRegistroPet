af35cb399f99c5b0c169202ea4df90446a6d1ba6
Bamarcheti <bmarchetifiorin@gmail.com>
2022-05-28 00:38:50 -0400

    registro pessoa

--- App.js
+++ App.js
@@ -1,12 +1,14 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 import { Provider as PaperProvider } from 'react-native-paper';
+import RegistroPessoa from './screens/registropessoa';
 import RegistroPet from "./screens/registropet";
 
 export default function App() {
 	return (
 		<PaperProvider>
 			<RegistroPet/>
+			<RegistroPessoa />
 		</PaperProvider>
 	);
 }
